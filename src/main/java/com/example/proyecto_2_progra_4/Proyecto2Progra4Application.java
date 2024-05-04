6176119a63b2a25abfcdb2c2a064f35307694523
Brayron Leiva <brayronleivas@gmail.com>
2024-05-04 10:04:03 -0600

    BEGIN

---
+++ src/main/java/com/example/proyecto_2_progra_4/Proyecto2Progra4Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_progra_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2Progra4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto2Progra4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyecto_2_progra_4/Proyecto2Progra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_2_progra_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2Progra4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
